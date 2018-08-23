c03f25e60fdeff4d9dfa9ecb93f224a30bb3e9d5
Colby Leiske <colby.leiske@gmail.com>
2018-08-22 17:04:51 -0700

    Added multi-lot support to API Caller

--- cronjobs/parkinglogixapicaller.go
+++ cronjobs/parkinglogixapicaller.go
@@ -11,6 +11,7 @@ import (
 	"github.com/colbyleiske/slugspace/utils"
 
 	"fmt"
+
 	_ "github.com/go-sql-driver/mysql"
 )
 
@@ -31,37 +32,39 @@ func main() {
 		Timeout: 2 * time.Second,
 	}
 
-	request, err := http.NewRequest(http.MethodGet, utils.ParkingLogixAPIURL, nil)
-	if err != nil {
-		panic(err)
+	for _, URL := range utils.ParkingLogixAPIURL {
+		request, err := http.NewRequest(http.MethodGet, URL, nil)
+		if err != nil {
+			panic(err)
+		}
+
+		request.Header.Add(utils.ParkingLogixAPIHeader, utils.ParkingLogixAPIKey)
+		request.Header.Set("User-Agent", "slugspace-data-gatherer")
+
+		response, err := httpClient.Do(request)
+		if err != nil {
+			panic(err)
+		}
+
+		body, err := ioutil.ReadAll(response.Body)
+		if err != nil {
+			panic(err)
+		}
+
+		parkingLotData := parkingLotData{}
+		if err = json.Unmarshal(body, &parkingLotData); err != nil {
+			panic(err)
+		}
+
+		stmt, err := db.Prepare(utils.UpdateLotInfoByName)
+		if err != nil {
+			panic(err)
+		}
+
+		if _, err = stmt.Exec(parkingLotData[0].FreeSpaces, parkingLotData[0].DateTime, parkingLotData[0].LocationName); err != nil {
+			panic(err)
+		}
+
+		fmt.Printf("Updated %s at %v\n", parkingLotData[0].LocationName, time.Now())
 	}
-
-	request.Header.Add(utils.ParkingLogixAPIHeader, utils.ParkingLogixAPIKey)
-	request.Header.Set("User-Agent", "slugspace-data-gatherer")
-
-	response, err := httpClient.Do(request)
-	if err != nil {
-		panic(err)
-	}
-
-	body, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		panic(err)
-	}
-
-	parkingLotData := parkingLotData{}
-	if err = json.Unmarshal(body, &parkingLotData); err != nil {
-		panic(err)
-	}
-
-	stmt, err := db.Prepare(utils.UpdateLotInfoByName)
-	if err != nil {
-		panic(err)
-	}
-
-	if _, err = stmt.Exec(parkingLotData[0].FreeSpaces, parkingLotData[0].DateTime, parkingLotData[0].LocationName); err != nil {
-		panic(err)
-	}
-
-	fmt.Printf("Updated at %v\n", time.Now())
 }
